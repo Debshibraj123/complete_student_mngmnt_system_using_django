1db5cbde3c8a54be909b2f38d553e99215bc8c1b
Shibraj <shibrajdeb456@gmail.com>
2024-01-18 01:14:25 +0530

    updated sidebar

--- mysite/mysite/Hod_Views.py
+++ mysite/mysite/Hod_Views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
-@login_required(login_url='/')
+# @login_required
 def HOME(request):
     return render(request,'HOD/home.html')
